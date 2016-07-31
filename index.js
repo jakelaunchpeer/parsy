64d553bcea334b45c7923f754524bb8b17c25d66
Timothy Barrett <liltimtim@gmail.com>
2016-07-31 12:52:40 -0400

    changed live query endpoints

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ var api = new ParseServer({
   masterKey: process.env.MASTER_KEY || 'Timoshii1!', //Add your master key here. Keep it secret!
   serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
   liveQuery: {
-    classNames: ["Request", "Tracking"] // List of classes to support for query subscriptions
+    classNames: ["Request", "LocationTracking"] // List of classes to support for query subscriptions
   }
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
